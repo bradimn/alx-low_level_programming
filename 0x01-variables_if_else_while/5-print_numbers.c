f9533b41be46d27f4205168167319ac6816ec760
bradimn <asonimarwan@gmail.com>
2023-03-18 04:35:26 -0700

    print nim5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
