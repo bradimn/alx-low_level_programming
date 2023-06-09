b231bc2963c19e19ba724d8454edf3dbc0d0364a
bradimn <asonimarwan@gmail.com>
2023-06-09 05:05:30 -0700

    whats your name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,9 @@
  * return: 0
  */
 
-int main(int argc, char **argv){
-	
-		printf("%s\n", argv[]);
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
 	return (0);
 }
